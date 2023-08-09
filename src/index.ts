7a951eb000bdc784c0cdd0ac918325e0fd18466b
jwstanly <jwstanly@yahoo.com>
2023-08-08 22:46:15 -0400

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,24 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler deploy src/index.ts --name my-worker` to deploy your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 import { forwardEventToGoogleAnalytics4 } from './ga';
 import { storeEventInCloudflareKV } from './kv';
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // Binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
   TRACKING: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
 
+  // Environment variables. Learn more at https://developers.cloudflare.com/workers/platform/environment-variables/
   GA4_MEASUREMENT_ID: string;
   GA4_API_SECRET: string;
 }
+
+export interface Event {
+  source: string;
+  ipAddress: string;
+  location: string;
+  time: string;
+}
+
+export const SOURCE_PARAM = 'k';
+export const DEFAULT_SOURCE = 'default';
 
 export default {
   async fetch(
@@ -34,15 +26,15 @@ export default {
     env: Env,
     ctx: ExecutionContext,
   ): Promise<Response> {
-    const source =
-      new URL(request.url).searchParams.get(SOURCE_PARAM) || DEFAULT_SOURCE;
-    const ipAddress = request.headers.get('CF-Connecting-IP')!;
-    const location = request.cf
-      ? `${request.cf.city}, ${request.cf.region}, ${request.cf.country}`
-      : 'Unknown';
-    const time = new Date().toLocaleString();
-
-    const event: Event = { source, ipAddress, location, time };
+    const event: Event = {
+      source:
+        new URL(request.url).searchParams.get(SOURCE_PARAM) || DEFAULT_SOURCE,
+      ipAddress: request.headers.get('CF-Connecting-IP')!,
+      location: request.cf
+        ? `${request.cf.city}, ${request.cf.region}, ${request.cf.country}`
+        : 'Unknown',
+      time: new Date().toLocaleString(),
+    };
 
     await Promise.all([
       forwardEventToGoogleAnalytics4({
@@ -56,32 +48,24 @@ export default {
       }),
     ]);
 
-    return new Response(TRANSPARENT_1X1_PIXEL, {
-      headers: {
-        'Content-Type': 'image/png',
-        'Cache-Control': 'no-cache, no-store, must-revalidate',
-        Pragma: 'no-cache',
-        Expires: '0',
+    // Transparent 1x1 PNG image
+    return new Response(
+      new Uint8Array([
+        0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d,
+        0x49, 0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+        0x08, 0x06, 0x00, 0x00, 0x00, 0x1f, 0x15, 0xc4, 0x89, 0x00, 0x00, 0x00,
+        0x0a, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9c, 0x63, 0x00, 0x01, 0x00, 0x00,
+        0x05, 0x00, 0x01, 0x0d, 0x0a, 0x2d, 0xb4, 0x00, 0x00, 0x00, 0x00, 0x49,
+        0x45, 0x4e, 0x44, 0xae, 0x42, 0x60, 0x82,
+      ]),
+      {
+        headers: {
+          'Content-Type': 'image/png',
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          Pragma: 'no-cache',
+          Expires: '0',
+        },
       },
-    });
+    );
   },
 };
-
-export interface Event {
-  source: string;
-  ipAddress: string;
-  location: string;
-  time: string;
-}
-
-const SOURCE_PARAM = 'k';
-const DEFAULT_SOURCE = 'default';
-
-const TRANSPARENT_1X1_PIXEL = new Uint8Array([
-  0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, 0x00, 0x00, 0x00, 0x0d, 0x49,
-  0x48, 0x44, 0x52, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06,
-  0x00, 0x00, 0x00, 0x1f, 0x15, 0xc4, 0x89, 0x00, 0x00, 0x00, 0x0a, 0x49, 0x44,
-  0x41, 0x54, 0x78, 0x9c, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01, 0x0d,
-  0x0a, 0x2d, 0xb4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4e, 0x44, 0xae, 0x42,
-  0x60, 0x82,
-]);
